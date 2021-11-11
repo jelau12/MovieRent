c7b79c831040766eff630686fde8f14f531052c5
Jesper Lauridsen <Jelauwow@gmail.com>
2021-11-11 14:27:46 +0100

    added dto

---
+++ MovieRenting/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using MovieRenting.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRenting.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
